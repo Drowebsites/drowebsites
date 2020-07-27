c337ac8303f4bc704b18c8684d4e52bfd665b140
Steveen <steveen.echeverri@gmail.com>
2020-07-26 23:25:07 -0500

    Created component Services first

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Hero from './Components/Hero/Hero';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import About from './Components/About/About';
 import Footer from './Components/Footer/Footer';
+import Services from './Components/Services/Services';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Hero />
       <main id="main">
         <About />
+        <Services />
       </main>
       <header className="App-header">
         <img src={logo} alt="logo" />
